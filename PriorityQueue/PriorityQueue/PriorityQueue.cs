0ab72a6085a1cebc75745dea66fe60570ecfee0a
TheLittleCitizen9 <thelittlecitizen9@gmail.com>
2020-08-13 09:56:50 +0300

    Created a priority queue

---
+++ PriorityQueue/PriorityQueue/PriorityQueue.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using PriorityQueue.Abstract;
+
+namespace PriorityQueue
+{
+    public class PriorityQueue<T> : IPriorityQueue<T>
+    {
+        public List<Tuple<uint, T>> PriorityList = new List<Tuple<uint, T>>();
+        public int Count => PriorityList.Count;
+
+        public T Dequeue()
+        {
+            var min = PriorityList.Min(r => r.Item1);
+            var newPriorityList = PriorityList.Where(l => l.Item1 == min).ToList();
+            PriorityList.Remove(newPriorityList[0]);
+            return newPriorityList[0].Item2;
+        }
+
+        public void Enqueue(uint priority, T item)
+        {
+            
+
+            PriorityList.Add(new Tuple<uint, T>(priority, item));
+        }
+    }
+}
---
+++ PriorityQueue/PriorityQueue/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace PriorityQueue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            PriorityQueue<Char> priorityQueue = new PriorityQueue<char>();
+
+            priorityQueue.Enqueue(1, 'A');
+            priorityQueue.Enqueue(2, 'B');
+            priorityQueue.Enqueue(1, 'C');
+
+            Console.WriteLine(priorityQueue.Count);
+            Console.WriteLine(priorityQueue.Dequeue());
+            Console.WriteLine(priorityQueue.Dequeue());
+            Console.WriteLine(priorityQueue.Dequeue());
+        }
+    }
+}
